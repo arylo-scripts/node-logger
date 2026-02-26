7f117ca8bda594108a398b23ed64673f569e8031
Arylo Yeung <arylo.open@gmail.com>
2026-02-27 04:05:27 +0800

    refactor(monkey): split shared utilities into workspace packages

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { AsyncLocalStorage } from 'async_hooks'
+import lodash from 'lodash'
+import { headParams } from '@scripts/function-control'
+
+type CustomConsole = {
+  log: typeof console.log,
+  debug: typeof console.log,
+  info: typeof console.info,
+  warn: typeof console.warn,
+  error: typeof console.error,
+}
+
+const storage = new AsyncLocalStorage<CustomConsole>()
+
+function inject<F extends (...args: any[]) => any>(names: string | string[], cb: F) {
+  const flag = lodash.castArray(names).map((name) => `[${name}]`).join('')
+  return storage.run({
+    log: headParams(console.log, flag),
+    debug: headParams(console.log, flag),
+    info: headParams(console.info, flag),
+    warn: headParams(console.warn, flag),
+    error: headParams(console.error, flag),
+  }, cb)
+}
+
+export const logger = {
+  log: (...args: any[]) => (storage.getStore()?.log ?? console.log)(...args),
+  debug: (...args: any[]) => (storage.getStore()?.debug ?? console.log)(...args),
+  info: (...args: any[]) => (storage.getStore()?.info ?? console.info)(...args),
+  warn: (...args: any[]) => (storage.getStore()?.warn ?? console.warn)(...args),
+  error: (...args: any[]) => (storage.getStore()?.error ?? console.error)(...args),
+  inject,
+}
+
+export default logger
