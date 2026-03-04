3d1d5860f7615cf4e4b16026edfd74448d254f91
Arylo Yeung <arylo.open@gmail.com>
2026-03-04 16:54:11 +0800

    chore(monkey,qinglong): migrate build pipeline to workspace packages
    - split build logic into workspace packages under packages/build-scripts
    - add qinglong workspace package and move qinglong sources into src/
    - introduce esbuild plugin packages and shared build utilities
    - update root/workspace scripts, turbo tasks, and GitHub workflows
    - align package lint/build configs and exports across monorepo

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
 import { AsyncLocalStorage } from 'async_hooks'
-import lodash from 'lodash'
 import { headParams } from '@scripts/function-control'
+import lodash from 'lodash'
 
 type CustomConsole = {
-  log: typeof console.log,
-  debug: typeof console.log,
-  info: typeof console.info,
-  warn: typeof console.warn,
-  error: typeof console.error,
+  log: typeof console.log
+  debug: typeof console.log
+  info: typeof console.info
+  warn: typeof console.warn
+  error: typeof console.error
 }
 
 const storage = new AsyncLocalStorage<CustomConsole>()
 
 function inject<F extends (...args: any[]) => any>(names: string | string[], cb: F) {
-  const flag = lodash.castArray(names).map((name) => `[${name}]`).join('')
-  return storage.run({
-    log: headParams(console.log, flag),
-    debug: headParams(console.log, flag),
-    info: headParams(console.info, flag),
-    warn: headParams(console.warn, flag),
-    error: headParams(console.error, flag),
-  }, cb)
+  const flag = lodash
+    .castArray(names)
+    .map((name) => `[${name}]`)
+    .join('')
+  return storage.run(
+    {
+      log: headParams(console.log, flag),
+      debug: headParams(console.log, flag),
+      info: headParams(console.info, flag),
+      warn: headParams(console.warn, flag),
+      error: headParams(console.error, flag),
+    },
+    cb,
+  )
 }
 
 export const logger = {
